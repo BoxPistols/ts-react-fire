781a09d703225ff67a57fea26b23e7cc367be577
aito <BoxPistols@users.noreply.github.com>
2022-08-14 05:14:38 +0900

    Refactor files

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { db } from "./firebase";
+import React, { useState, useEffect } from "react"
+import "./style/App.css"
+import { db } from "./firebase"
 
-const authDomain = process.env.REACT_APP_FIREBASE_DOMAIN;
-console.log(authDomain);
+const authDomain = process.env.REACT_APP_FIREBASE_DOMAIN
+console.log(authDomain)
 
-console.log(db);
+console.log(db)
 
 const App: React.FC = () => {
-
-  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [tasks, setTasks] = useState([{ id: "", title: "" }])
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
         snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
-      );
-    });
-    return () => unSub();
-  }, []);
+      )
+    })
+    return () => unSub()
+  }, [])
 
   return (
     <>
-        App
-      <div className="App" >
-        {tasks.map((task) => <h3 key="task.id">{task.title}</h3>)}
+      App
+      <div className="App">
+        {tasks.map((task) => (
+          <h3 key="task.id">{task.title}</h3>
+        ))}
       </div>
     </>
-  );
-};
-export default App;
+  )
+}
+export default App

6ce6765a380143553308fea87f975cfe8f3c4440
ait <ait@ai.in>
2022-08-20 23:19:15 +0900

    Add Router / Add login component

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,18 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './style/index.css'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>,
 )

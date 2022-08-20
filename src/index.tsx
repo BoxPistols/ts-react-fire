2f41b4129f3bcf3dab11f55627bbde68d6327b87
aito <BoxPistols@users.noreply.github.com>
2022-08-21 07:17:09 +0900

    Auth Login User Base

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './style/index.css'
 
+// @note: npm i --save-dev @types/react-router-dom
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
+import { Login } from './pages/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -13,7 +14,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login history={[]} />}></Route>
       </Routes>
     </BrowserRouter>
     {/* <App /> */}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,75 @@
-const Login = () => {
+import { Button, FormControl, TextField } from '@material-ui/core'
+import { useEffect, useState } from 'react'
+// useState
+import auth from '../firebase'
+import styles from '../style/Login.module.css'
+
+export const Login = (props: any) => {
+  const [isLogin, SetIsLogin] = useState(true)
+  const [email, SetEmail] = useState('')
+  const [password, SetPassword] = useState('')
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      user && props.history.push('/')
+    })
+  }, [props.history])
+
   return (
     <>
-      <div>Login</div>
+      <div className={styles.login}>
+        <h2>{isLogin ? 'Login' : 'Resister'}</h2>
+        <br />
+        <FormControl>
+          <TextField
+            name="email"
+            label="E-mail"
+            value={email}
+            onChange={(
+              e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+            ) => {
+              SetEmail(e.target.value)
+            }}
+          />
+          <TextField
+            name="password"
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(
+              e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+            ) => {
+              SetPassword(e.target.value)
+            }}
+          />
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={
+              isLogin
+                ? async () => {
+                    try {
+                      await auth.signInWithEmailAndPassword(email, password)
+                      props.history.push('/')
+                    } catch (error: any) {
+                      alert(error.message)
+                    }
+                  }
+                : async () => {
+                    try {
+                      await auth.createUserWithEmailAndPassword(email, password)
+                      props.history.push('/')
+                    } catch (error: any) {
+                      alert(error.message)
+                    }
+                  }
+            }
+          >
+            {isLogin ? 'Login' : 'Resister'}
+          </Button>
+        </FormControl>
+      </div>
     </>
   )
 }
-
-export default Login
